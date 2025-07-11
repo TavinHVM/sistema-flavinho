9d5a1e6b4a01149abe4950830579ac2de285789f
TavinHVM <gustav0.henriqu31504@gmail.com>
2025-07-11 14:28:48 -0300

    feat: date formatting

--- components/OrderDetailsModal.tsx
+++ components/OrderDetailsModal.tsx
@@ -100,9 +100,9 @@ const OrderDetailsModal: React.FC<Props> = ({ pedido, open, onClose, onEditar, o
           <div><b>Endereço Evento:</b> {pedido.endereco}</div>
           <div><b>Endereço Residencial:</b> {pedido.residencial}</div>
           <div><b>Referência:</b> {pedido.referencia}</div>
-          <div><b>Entrega:</b> {pedido.data_entregou} às {pedido.horario_entregou}</div>
-          <div><b>Busca:</b> {pedido.data_buscou} às {pedido.horario_buscou}</div>
-          <div><b>Recebeu:</b> {pedido.data_recebeu} às {pedido.horario_recebeu}</div>
+          <div><b>Entrega:</b> {formatDateBR(pedido.data_entregou)} às {pedido.horario_entregou}</div>
+          <div><b>Busca:</b> {formatDateBR(pedido.data_buscou)} às {pedido.horario_buscou}</div>
+          <div><b>Recebeu:</b> {formatDateBR(pedido.data_recebeu)} às {pedido.horario_recebeu}</div>
           <div><b>Pagamento:</b> {pedido.pagamento}</div>
           <div><b>Valor Pago:</b> R$ {pedido.valor_pago?.toFixed(2)}</div>
           <div><b>Desconto:</b> R$ {pedido.desconto?.toFixed(2)}</div>
@@ -113,9 +113,9 @@ const OrderDetailsModal: React.FC<Props> = ({ pedido, open, onClose, onEditar, o
           <OrderMaterialsList materiais={pedido.materiais} />
         </div>
         <div className="mt-4 grid grid-cols-1 md:grid-cols-2 gap-2 text-xs text-white">
-          <div><b>Resp. Entregou:</b> {pedido.responsavel_entregou} <b>Data:</b> {formatDateBR(pedido.data_entregou)}</div>
-          <div><b>Resp. Recebeu:</b> {pedido.responsavel_recebeu} <b>Data:</b> {formatDateBR(pedido.data_recebeu)}</div>
-          <div><b>Resp. Buscou:</b> {pedido.responsavel_buscou} <b>Data:</b> {formatDateBR(pedido.data_buscou)}</div>
+          <div><b>Resp. Entregou:</b> {pedido.responsavel_entregou}</div>
+          <div><b>Resp. Recebeu:</b> {pedido.responsavel_recebeu}</div>
+          <div><b>Resp. Buscou:</b> {pedido.responsavel_buscou}</div>
           <div><b>Resp. Conferiu Forro:</b> {pedido.responsavel_conferiu_forro}</div>
           <div><b>Resp. Conferiu Utensílio:</b> {pedido.responsavel_conferiu_utensilio}</div>
         </div>
